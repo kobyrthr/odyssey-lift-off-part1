0881debd4cda00a2dadce74bf4ace4564db81644
kobyrthr-dev <kobyrthr@gmail.com>
2023-03-26 17:23:21 -0400

    rendering query data in react card component

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { Layout } from '../components';
+import {gql,useQuery} from "@apollo/client"
+import TrackCard from '../containers/track-card';
+
+const TRACKS = gql`
+query GetTracks {
+  tracksForHome {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+  }
+}
+
+`
+
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const {loading,error,data} = useQuery(TRACKS)
+  if (loading) return 'Loading...'
+
+  if (error) return `ERROR! ${error.message}`
+
+  return <Layout grid> {data?.tracksForHome?.map((track)=>(
+    <TrackCard key={track.id} track={track}></TrackCard>
+  ))}</Layout>;
 };
 
 export default Tracks;
